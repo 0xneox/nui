59bf65b8abc8af3d487294e22002f3c88c10c67d
binarybodhi <neohex262@gmail.com>
2024-09-23 21:43:10 +0530

    Add files via upload

---
+++ build/static/js/724.7d9f5dee.chunk.js
@@ -0,0 +1,7 @@
+"use strict";(self.webpackChunkneurolov_webapp=self.webpackChunkneurolov_webapp||[]).push([[724],{724:(e,r,s)=>{s.r(r),s.d(r,{default:()=>l});s(43);var a=s(867),n=s(579);const i=a.Ay.div`
+  color: white;
+  padding: 20px;
+  max-width: 800px;
+  margin: 0 auto;
+`,l=()=>(0,n.jsxs)(i,{children:[(0,n.jsx)("p",{children:"Referral System FAQ"}),(0,n.jsx)("p",{children:"How does the referral system work?"}),(0,n.jsx)("h4",{children:"Our referral system rewards you for bringing new users to the platform. It works on three levels:"}),(0,n.jsxs)("ul",{children:[(0,n.jsx)("li",{children:"Primary Referrals: You get 10% of the XP earned by users you directly invite."}),(0,n.jsx)("li",{children:"Secondary Referrals: You get 5% of the XP earned by users invited by your primary referrals."}),(0,n.jsx)("li",{children:"Tertiary Referrals: You get 2.5% of the XP earned by users invited by your secondary referrals."})]}),(0,n.jsx)("p",{children:"Is there a limit to how much I can earn?"}),(0,n.jsx)("h4",{children:"There's no cap on your earnings! The more active your referral network, the more you can earn."}),(0,n.jsx)("h4",{children:"How often are referral rewards calculated?"}),(0,n.jsx)("h4",{children:"Referral rewards are calculated and distributed in real-time as your referrals earn XP."}),(0,n.jsx)("p",{children:"Can I track my referrals' progress?"}),(0,n.jsx)("h4",{children:"Yes, you can view your referrals and their earnings in the Friends List section."})]})}}]);
+//# sourceMappingURL=724.7d9f5dee.chunk.js.map
